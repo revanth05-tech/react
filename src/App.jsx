3a4bbe3180ccff166e65a18d4887c1a04b1e0285
revanth05-tech <revanthyegireddi@gmail.com>
2025-10-18 16:40:15 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Card from './component/card';
 import Navbar from './component/navbar';
+import './App.css'; // your external CSS file
 
 const App = () => {
   return (
-    <div>
-      <Card />
-      <Navbar />
+    <div className='parent'>
+      <div className="card">
+        <h1>Hello World</h1>
+        <button className="btn">
+          Click Me
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
