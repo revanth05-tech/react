cbc815d6f6c51bb477a98d04ee0665e04f8ffcaa
revanth05-tech <revanthyegireddi@gmail.com>
2025-10-22 20:39:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import React from 'react';
-import Card from './component/card';
-import Navbar from './component/navbar';
-import './App.css';
-import naruto from './images/naruto.avif'; // ✅ Import the image
+import React from "react";
+import Card from "./component/card";
+import naruto from "./images/naruto.avif";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='parent'>
-      <div className="card">
-        <img className='img_card' src={naruto} alt="Naruto" />
-        <h1>Hello World</h1>
-        <button className="btn">
-          Click Me
-        </button>
-      </div>
+    <div className="container">
+      <Card name="Naruto Uzumaki" age={17} img={naruto} />
     </div>
   );
-}
+};
 
 export default App;
--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ name, age, img }) => {
   return (
-    <div>
-      <h2>Card Title</h2>
-      <p>This is a description of the card.</p>
+    <div className="parent">
+      <div className="card">
+        <img className="img_card" src={img} alt={name} />
+        <h1>{name}</h1>
+        <p>Age: {age}</p>
+        <button className="btn">Click Me</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
