4ded065f372c6e97faa190b5e54318735e4b8c59
revanth05-tech <revanthyegireddi@gmail.com>
2025-10-24 22:12:38 +0530

    updated card

--- Cards_project/src/App.jsx
+++ Cards_project/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Bookmark } from 'lucide-react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='parent'>
+      <div className='card'>
+        <div className='google'>
+          <div className='navbar'>
+            <a href="https://www.google.com/">
+              <img
+                className='logo'
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
+                alt="Google Logo"
+              />
+            </a>
+            <button className='btn'>
+              Save <Bookmark size={18} style={{ marginLeft: '6px' }} />
+            </button>
+          </div>
+          <div className='content'>
+            <a
+              href="https://www.google.com/about/careers/applications/jobs/results/116498926541906630-senior-ux-designer/"
+              className='title'
+            >
+              Google
+            </a>
+            <h2 className='role'>Senior UI/UX Designer</h2>
+          </div>
+        </div>
+        <div className='center'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
