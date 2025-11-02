ee4363303e5079ab8f3547d12ef5e80487554022
revanth05-tech <revanthyegireddi@gmail.com>
2025-11-02 14:29:13 +0530

    updated card and user

--- cardspro/src/App.jsx
+++ cardspro/src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 import Card from './components/card.jsx';
+import User from './components/user.jsx';
 
 const App = () => {
+
+const arr = [
+  { name: 'Alice' },
+  {age : 30}
+];
+  
   return (
     <div className='parent'>
-      <Card />
-       <Card />
-        <Card />
+      {arr.map(function(char){
+        return char.age;
+      })}
     </div>
-    
-  );
+  )
 };
 
 export default App;
